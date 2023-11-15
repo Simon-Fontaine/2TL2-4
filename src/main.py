4fdf9c5944016370a465844d752299e2394d3765
Simon Fontaine <74004726+Simon-Fontaine@users.noreply.github.com>
2023-11-15 13:27:29 +0100

    modified the display, made it tables

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import time
-import argparse
-from termcolor import colored, cprint
+
+from rich.console import Console
+from rich.table import Table
+from rich.live import Live
+from rich.prompt import Confirm, IntPrompt
+
 from classes.colony import Colony
 from classes.settings import (
     MAX_ANT_AGE,
@@ -10,72 +14,93 @@ from classes.settings import (
     EGG_HATCHING_RATE,
 )
 
-
-def print_header(text):
-    """
-    Affiche un texte en vert et en gras.
-    """
-    print(colored(text, "green", attrs=["bold"]))
+console = Console()
 
 
-def print_separator():
+def create_table(title, header_style="bold cyan", column_styles=("bold blue", "yellow")):
     """
-    Affiche un séparateur jaune.
+    Créer une table avec un titre et des styles de colonnes et d'en-tête par défaut.
     """
-    print(colored("=" * 40, "yellow"))
+    table = Table(title=title, show_header=True, header_style=header_style, title_style="bold magenta", min_width=50)
+    table.add_column("Métrique", style=column_styles[0])
+    table.add_column("Valeur", style=column_styles[1])
+    return table
+
+
+def print_parameters(ants: int, food: int, speed: int) -> Table:
+    """
+    Affiche les paramètres de la simulation.
+    """
+    table = create_table("Paramètres de la Simulation")
+    table.add_row("Vitesse (secondes/jour)", str(speed))
+    table.add_row("Fourmis Initiales", str(ants))
+    table.add_row("Nourriture Initiale", str(food))
+    return table
+
+
+def print_simulation_details() -> Table:
+    """
+    Affiche les détails de la simulation.
+    """
+    table = create_table("Détails de la Simulation")
+    table.add_row("Âge Max des Fourmis (jours)", str(MAX_ANT_AGE))
+    table.add_row("Âge Max de la Reine (jours)", str(MAX_QUEEN_AGE))
+    table.add_row("Taux de Ponte de la Reine (œufs/jour)", str(QUEEN_EGG_LAYING_RATE))
+    table.add_row("Temps de Croissance des Œufs (jours)", str(EGG_GROWING_TIME))
+    table.add_row("Taux d'Éclosion des Œufs (%)", f"{EGG_HATCHING_RATE * 100}")
+    return table
+
+
+def print_simulation_results(colony) -> Table:
+    """
+    Affiche les résultats de la simulation.
+    """
+    table = create_table("Résultats de la Simulation")
+    table.add_row("Temps Total", str(colony.days_to_years_months_days))
+    table.add_row("Jours", str(colony.day))
+    table.add_row("Œufs", str(len(colony.eggs)))
+    table.add_row("Fourmis", str(colony.ant_count))
+    table.add_row("Nourriture", str(colony.food.quantity))
+    table.add_row("Reine", "Vivante" if colony.queen.is_alive else "Décédée")
+    table.add_row("Fourmis Mortes", str(colony.dead_ant_count))
+    return table
 
 
 def main():
     """
     Fonction principale du programme.
     """
-    parser = argparse.ArgumentParser(description="Simuler une colonie de fourmis")
+    console.print("Bienvenue dans le simulateur de colonie de fourmis!", style="bold green")
 
-    parser.add_argument("ants", help="Le nombre initial de fourmis", type=int)
-    parser.add_argument("food", help="La quantité initiale de nourriture", type=int)
-    parser.add_argument(
-        "--speed",
-        help="La vitesse de simulation en secondes par jour (défaut = 1 secondes/jour)",
-        default=1,
-        type=float,
-    )
+    ants = IntPrompt.ask("Entrez le nombre initial de fourmis", default=100)
+    food = IntPrompt.ask("Entrez la quantité initiale de nourriture", default=10000)
+    speed = IntPrompt.ask("Entrez la vitesse de la simulation en secondes par jour", default=0)
 
-    args = parser.parse_args()
+    console.print("Récapitulatif des Paramètres de Simulation:")
+    console.print(print_parameters(ants, food, speed))
+    console.print("Détails de la Simulation:")
+    console.print(print_simulation_details())
 
-    print_header("Paramètres de la simulation :")
-    print_separator()
-    cprint(f"Vitesse : {args.speed} secondes/jour", "blue")
-    cprint(f"Fourmis initiales : {args.ants}", "blue")
-    cprint(f"Nourriture initiale : {args.food}", "blue")
-    print_separator()
-    cprint(f"Âge maximal des fourmis : {MAX_ANT_AGE} jours", "cyan")
-    cprint(f"Âge maximal de la reine : {MAX_QUEEN_AGE} jours", "cyan")
-    cprint(f"Production d'œufs de la reine : {QUEEN_EGG_LAYING_RATE} œufs/jour", "cyan")
-    cprint(f"Temps d'incubation des œufs : {EGG_GROWING_TIME} jours", "cyan")
-    cprint(f"Taux d'éclosion des œufs : {EGG_HATCHING_RATE * 100}%", "cyan")
-    print_separator()
+    if Confirm.ask("Voulez-vous lancer la simulation maintenant ?"):
+        colony = Colony(ants, food)
 
-    input(colored("Appuyez sur Entrée pour commencer la simulation...", "red", attrs=["bold"]))
+        with Live(auto_refresh=False) as live:
+            while colony.ants or colony.queen.is_alive or colony.eggs:
+                colony.update()
+                live.update(print_simulation_results(colony))
+                live.refresh()
+                time.sleep(speed)
 
-    colony = Colony(args.ants, args.food)
+        console.print("[bold green]Simulation terminée. Voici les résultats finaux :[/]")
+        console.print(print_simulation_results(colony))
+    else:
+        console.print("[bold red]Simulation annulée.[/]")
 
-    while colony.ants or colony.queen.is_alive or colony.eggs:
-        colony.update()
-        print_separator()
-        cprint(colony.days_to_years_months_days, "magenta", attrs=["bold"])
-        cprint(f"Total: {colony.day} jours\n", "blue", attrs=["bold"])
-        cprint(f"Oeufs: {len(colony.eggs)}", "cyan")
-        cprint(f"Fourmis: {colony.ant_count}", "cyan")
-        cprint(f"Nourriture: {colony.food.quantity}", "cyan")
-        cprint(
-            f"Reine vivante: {'Oui' if colony.queen.is_alive else 'Non'}",
-            "green" if colony.queen.is_alive else "red",
-        )
-        cprint(f"Fourmis mortes: {colony.dead_ant_count}", "cyan")
-        print_separator()
-        time.sleep(args.speed)
-
-    cprint("La simulation est terminée.", "magenta", attrs=["bold"])
+    if Confirm.ask("Voulez-vous quitter le programme ?"):
+        console.print("[bold green]Au revoir ![/]")
+    else:
+        console.print("[bold red]Retour au menu principal...[/]")
+        main()
 
 
 if __name__ == "__main__":
